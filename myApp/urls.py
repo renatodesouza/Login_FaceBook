9e216f8401c67da278cfccabeee0f621be90f43c
renato <rlucas.rs2@gmail.com>
2021-02-13 13:34:08 -0300

    pronto para o heroku

---
+++ myApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from .views import LoginView, IndexView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+]
---
+++ myApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+
