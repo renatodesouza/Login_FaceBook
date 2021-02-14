45b04ce70972e698eede0d4015e07f0c84861447
renato <rlucas.rs2@gmail.com>
2021-02-14 18:47:45 -0300

    Login e logout com django ok

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from .views import LoginView, IndexView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import IndexView, UserTemplateView, AutenticacaoView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('auth/', AutenticacaoView.as_view(), name='auth'),
+    path('login/', LoginView.as_view(template_name='user.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='index.html'), name='logout'),
+    path('user/', UserTemplateView.as_view(), name='user'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('social-auth/', include('social_django.urls', namespace='social')),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, DetailView
 
-from django.views.generic import TemplateView
 
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class LoginView(TemplateView):
+class AutenticacaoView(TemplateView):
     template_name = 'login.html'
+
+class UserTemplateView(LoginRequiredMixin, TemplateView):
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
+    template_name = 'user.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users'] = User.objects.all()
+        return context
+
+
+def login_user(request):
+    username = request.GET['username']
+    password = request.GET['password']
+
+    user = authenticate(request, username=username, password=password)
+    print("Usuario: ", user)
+    if user is not None:
+        login(request, user)
+        return redirect('user')
+    else:
+        context = 'login invalido'
+        return render(request, 'index.html', {'context':context})
+
 
 
